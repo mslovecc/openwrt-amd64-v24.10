754c3ff8e0c757715cc1dd4b0b61997bbfea3b9c
mslovecc <33970221+mslovecc@users.noreply.github.com>
2026-01-15 13:31:28 +0800

    Initial commit

---
+++ lede/copy-amd64.sh
@@ -0,0 +1,6 @@
+cp $GITHUB_WORKSPACE/lede/shadow $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/shadow
+#cp $GITHUB_WORKSPACE/lede/hosts $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/hosts
+#cp $GITHUB_WORKSPACE/lede/config_generate $GITHUB_WORKSPACE/openwrt/package/base-files/files/bin/config_generate
+#cp $GITHUB_WORKSPACE/lede/mac80211.sh $GITHUB_WORKSPACE/openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#cp $GITHUB_WORKSPACE/lede/dhcp.conf $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/files/dhcp.conf
+#cp $GITHUB_WORKSPACE/lede/dnsmasq.conf $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/files/dnsmasq.conf
